2c887a7501436e6af1a1a6720e94a27444d17dd9
Mircus58 <spagnolo_mg@outlook.com>
2025-04-14 09:54:23 +0200

    lastv

--- src/app/containers/search-page/search-page.component.ts
+++ src/app/containers/search-page/search-page.component.ts
@@ -1,15 +1,47 @@
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ApiCallService } from '../../services/api-call.service';
 import { RouteResponse } from '../../../interfaces';
 import { FormatDatePipe } from '../../pipes/format-date.pipe';
-import { IonicModule } from '@ionic/angular';
 
+// Ionic standalone components
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonDatetime,
+  IonText,
+  IonButton,
+} from '@ionic/angular/standalone';
+
+
+const ui = [
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonDatetime,
+  IonText,
+  IonButton,
+];
 
 @Component({
   selector: 'app-search-page',
-  imports: [CommonModule, FormatDatePipe,ReactiveFormsModule, IonicModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormatDatePipe,
+    ...ui, 
+  ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
@@ -17,7 +49,7 @@ export class SearchPageComponent {
   private formatDate = inject(FormatDatePipe);
   form: FormGroup;
 
-  public result : RouteResponse | undefined;
+  public result: RouteResponse | undefined;
 
   constructor(
     private fb: FormBuilder,
@@ -31,18 +63,18 @@ export class SearchPageComponent {
     });
   }
 
- async onSubmit(): Promise<void> {
-  if (this.form.valid) {
-    const raw = this.form.value;
+  async onSubmit(): Promise<void> {
+    if (this.form.valid) {
+      const raw = this.form.value;
 
-    const formattedParams = {
-      ...raw,
-      date: this.formatDate.transform(raw.date) // 👈 transforme ISO → 4/8/2025
-    };
+      const formattedParams = {
+        ...raw,
+        date: this.formatDate.transform(raw.date) // 👈 ISO → DD/MM/YYYY
+      };
 
-    const result = await this.api.getRoute(formattedParams);
-    console.log(result);
-    this.result = result;
+      const result = await this.api.getRoute(formattedParams);
+      console.log(result);
+      this.result = result;
+    }
   }
-}
 }
